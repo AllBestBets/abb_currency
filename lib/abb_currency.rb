85516af7987bee396b9a007a55b813ab89aa15a0
valnech <valnech.m@gmail.com>
2017-02-09 11:28:54 +0100

    Save result for rates (#2)

--- lib/abb_currency.rb
+++ lib/abb_currency.rb
@@ -10,8 +10,8 @@ module AbbCurrency
     adapter = const_get(
       %W(AbbCurrency Adapters #{source.to_s.capitalize}).join("::")
     ).new
-    
-    adapter.rates.nil? ? nil : adapter.rates.merge('USD' => 1, 'adapter' => source.to_s.capitalize)
+    _rates = adapter.rates # save the result
+    _rates.nil? ? nil : _rates.merge('USD' => 1, 'adapter' => source.to_s.capitalize)
   end
 
   def self.rates
